e79ad8dc9cb5856b20d30391cd49ed802b706914
ilias <konesbradi@gmail.com>
2026-03-15 19:58:44 +0000

    Update app.py

--- app/app.py
+++ app/app.py
@@ -10,7 +10,7 @@ import shap
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
+
 import numpy as np
 
 # ─────────────────────────────────────────────────────────────────────────────
